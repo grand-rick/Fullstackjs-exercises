e62e78f1e4b4db503635d876fc53464f5e0c6574
Patrick Kabuga <kabugap02@gmail.com>
2023-01-10 03:14:35 +0300

    Created the article routes in the handler file and exported them into the server.ts file

--- nine-REST-APIs/src/server.ts
+++ nine-REST-APIs/src/server.ts
@@ -2,9 +2,7 @@ import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
-import ArticleStore, {Article} from './models/article';
-
-let article = new ArticleStore();
+import articles_routes from './handlers/articles';
 
 const app: express.Application = express();
 const port: number = 3000;
@@ -18,55 +16,12 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(morgan('common'));
 
+articles_routes(app);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('This is the homepage');
 });
 
-app.get('/article', (req: Request, res: Response) => {
-    res.send('This is the index route');
-});
-
-app.get('/article/:id', (req: Request, res: Response) => {
-    const id = req.params.id;
-    res.send(`This is the show route, showing article-${id}`);
-});
-
-app.post('/articles', (req: Request, res: Response) => {
-    const article: Article = {
-      title: req.body.title,
-      content: req.body.content
-    }
-    try {
-       res.send('this is the CREATE route')
-    } catch (err) {
-       res.status(400)
-       res.json(err)
-    }
-})
-
-app.put('/articles/:id', (req: Request, res: Response) => {
-    const article: Article = {
-      id: req.params.id, 
-      title: req.body.title,
-      content: req.body.content
-    }
-    try {
-       res.send('this is the EDIT route')
-    } catch (err) {
-       res.status(400)
-       res.json(err)
-    }
-})
-
-app.delete('/articles/:id', (_req: Request, res: Response) => {
-    try {
-       res.send('this is the DELETE route')
-    } catch (err) {
-       res.status(400)
-       res.json(err)
-    }
-})
 app.get('/test-cors', cors(corsOptions), (req: Request, res: Response) => {
     res.json({msg: 'This is CORS-enabled with a middle ware'});
 } )
